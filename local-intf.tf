38bc3f8b5f581a5a7f872ff2d6440c5c9524eaba
daisy-dynawhite <daledynawhite@outlook.com>
2025-03-18 20:24:11 +1000

    updates

--- local-intf.tf
+++ local-intf.tf
@@ -14,3 +14,19 @@ resource "iosxr_interface" "INT-SR2-Lo0"{
     ipv4_netmask = "255.255.255.255"
     provider = iosxr.SR-R2
 }
+resource "iosxr_interface" "INT-SR1-Gi1"{
+    interface_name = "GigabitEthernet0/0/0/1"
+    description = "*** SR1 Gi1 - SR2 Gi1 ***"
+    shutdown = false
+    ipv4_address = "10.1.1.0"
+    ipv4_netmask = "255.255.255.254"
+    provider = iosxr.SR-R1
+}
+resource "iosxr_interface" "INT-SR2-Gi1"{
+    interface_name = "GigabitEthernet0/0/0/1"
+    description = "*** SR2 Gi1 - SR1 Gi1 ***"
+    shutdown = false
+    ipv4_address = "10.1.1.1"
+    ipv4_netmask = "255.255.255.254"
+    provider = iosxr.SR-R2
+}
